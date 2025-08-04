f6541dd1631641d7a48dfa64dab03d54d5c1f053
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-08-04 22:21:08 +0530

    Create main.java

---
+++ intro software development/main.java
@@ -0,0 +1,6 @@
+public class Main {
+public static void main(String[] args) {
+        String[] superheroes = {"Batman", "Superman", "Wonder Woman", "Spider-Man", "Iron Man"};
+        int[] introductionYears = {1939, 1938, 1941, 1962, 1963};
+   }
+}
