944bfe79b63167f035d000afdd2c7f6b8d1fd08c
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-08-04 21:58:36 +0530

    Add files via upload

---
+++ intro software development/AssingnmentOperator2.java
@@ -0,0 +1,15 @@
+// Add numbers using += assignment operator
+public class AssingnmentOperator2 {
+    public static void main(String args[]) {
+
+        int itemsAvailable = 17;
+
+        System.out.println("Current available items : " + itemsAvailable);
+
+        // Adding 10 more items
+        itemsAvailable -=9;
+
+        System.out.println("Total available items after adding: " + itemsAvailable);
+
+    }
+}
---
+++ intro software development/carInfo.java
@@ -0,0 +1,17 @@
+public class carInfo {
+    public static void main(String[] args) {
+        String make = "Ford";
+        String model = "Mustang";
+        int Year = 2020;
+        double mileage = 15002.5;
+        boolean isAutomatic = true;
+
+        System.out.println("car make : " +make );
+        System.out.println("car model : " +model);
+        System.out.println("car year : " +Year);
+        System.out.println("car mileage : " +mileage);
+        System.out.println("car is Automatic : " +isAutomatic);
+
+    }
+
+}
