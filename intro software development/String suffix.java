a7f10ecaecfff74ead6131c7f8df9a30b187b3a0
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-08-05 14:26:01 +0530

    Create String suffix.java

---
+++ intro software development/String suffix.java
@@ -0,0 +1,28 @@
+//public class Main {
+//	public static void main(String[] args) {
+  //  	String testString = "This is a test";
+ 
+   // 	boolean result1 = testString.endsWith("test"); // true
+  //  	boolean result2 = testString.endsWith("This"); // false
+ 
+    //    System.out.println("testString.endsWith(\"test\") : " + testString.endsWith("test"));
+    //    System.out.println("testString.endsWith(\"This\") : " + testString.endsWith("This"));
+ 
+//	}
+//}
+
+//public class Main {
+	// public static void main(String[] args) {
+   // 	String testString1 = "This is a test";
+   // 	String testString2 = "";
+ 
+    //	boolean stringEmpty1 = testString1.isEmpty(); // false
+    //  boolean stringEmpty2 = testString2.isEmpty();  // true
+ 
+ 
+    //    System.out.println("testString1.isEmpty() : " + testString1.isEmpty());
+      //  System.out.println("testString2.isEmpty() : " + testString2.isEmpty());
+ 
+//	}
+//}
+
