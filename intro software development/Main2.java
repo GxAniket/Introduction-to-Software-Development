fca9d26c0e84787717b954714dad40f89d6bc3c9
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-08-05 14:15:12 +0530

    Create Main2.java

---
+++ intro software development/Main2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountofMoney = 550;
+
+        if (amountofMoney >= 300) {
+            System.out.println("Go to Las Vegas.");
+        } else if (amountofMoney >= 200) {
+            System.out.println("Go to Hoover Dam.");
+        } else if (amountofMoney >= 100) {
+            System.out.println("Go to Area 51.");
+        }
+    }
+}
